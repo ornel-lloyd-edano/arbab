4ae456590b504d65a982f4b08ee23f60044d0581
ornel-lloyd-edano <ornel.lloyd.edano@gmail.com>
2024-12-22 21:14:46 +0800

    first commit from Spring Initializer project template generator

---
+++ src/main/java/com/arbab/spaces/SpacesApplication.java
@@ -0,0 +1,13 @@
+package com.arbab.spaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arbab/spaces/SpacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbab.spaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
